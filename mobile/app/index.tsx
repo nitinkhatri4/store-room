b32834ac36a4579fcec93b48166ead73176c4b44
nitinkhatri4 <nitinkhatri1764@gmail.com>
2026-03-08 21:54:58 +0530

    icon changes

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -2,12 +2,16 @@ import { useEffect } from "react";
 import { useRouter } from "expo-router";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { View, ActivityIndicator } from "react-native";
+import * as SplashScreen from "expo-splash-screen";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function Index() {
   const router = useRouter();
 
   useEffect(() => {
     AsyncStorage.getItem("token").then((token) => {
+      SplashScreen.hideAsync(); // hide immediately
       if (token) {
         router.replace("/home");
       } else {
